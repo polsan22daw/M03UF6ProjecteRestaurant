5f6947fb40c8118a7d90d404205306946d83c45b
marped22daw <marped22daw@proton.me>
2023-05-08 20:03:22 +0200

    crea base de dades si no existeix

--- src/main/java/fje/edu/m03uf6projecterestaurant/ConnexioMesProves.java
+++ src/main/java/fje/edu/m03uf6projecterestaurant/ConnexioMesProves.java
@@ -32,6 +32,85 @@ public class ConnexioMesProves {
      */
     public static void executarTest() throws SQLException, IOException {
         Connection conn = obtenirConnexio();
+        ResultSet rs = null;
+        String NOMBRE_BD = "restaurant";
+        Statement stattt = conn.createStatement();
+        stattt.executeUpdate("CREATE DATABASE IF NOT EXISTS restaurant");
+        try {
+            conn = obtenirConnexio();
+
+            // Comprobar si la base de datos existe
+            rs = conn.getMetaData().getCatalogs();
+            boolean existsDatabase = false;
+            while (rs.next()) {
+                String databaseName = rs.getString(1);
+                if (databaseName.equalsIgnoreCase(NOMBRE_BD)) {
+                    existsDatabase = true;
+                    break;
+                }
+            }
+            rs.close();
+
+            if (!existsDatabase) {
+                // Crear la base de datos
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate("CREATE DATABASE " + NOMBRE_BD);
+                System.out.println("Base de datos " + NOMBRE_BD + " creada correctamente.");
+                stmt.close();
+            }
+
+            // Usar la base de datos
+            conn.setCatalog(NOMBRE_BD);
+
+            // Comprobar si la tabla existe
+            rs = conn.getMetaData().getTables(null, null, "plat", null);
+            boolean existsTable = rs.next();
+            rs.close();
+
+            if (!existsTable) {
+                // Crear la tabla
+                Statement stat = conn.createStatement();
+                stat.executeUpdate("CREATE TABLE plat (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(50), descripcio VARCHAR(200), preu DOUBLE, urlIMG VARCHAR(200), categoria VARCHAR(50), ingredients VARCHAR(200))");
+                System.out.println("Tabla 'plat' creada correctamente.");
+                stat.close();
+            }
+
+            // Insertar datos y mostrar la tabla
+            Statement stat = conn.createStatement();
+            /*stat.executeUpdate("INSERT INTO plat (nom, descripcio, preu, urlIMG, categoria, ingredients)"
+                    + "VALUES ('Plato 1', 'Descripción del plato 1', 10.99, 'https://example.com/image1.jpg', 'Primers', 'Ingrediente 1, Ingrediente 2')");
+            stat.executeUpdate("INSERT INTO plat (nom, descripcio, preu, urlIMG, categoria, ingredients)"
+                    + "VALUES ('Plato 2', 'Descripción del plato 2', 15.99, 'https://example.com/image2.jpg', 'Segons', 'Ingrediente 3, Ingrediente 4')");
+            System.out.println("Valores insertados en la tabla 'plat' correctamente.");*/
+
+            ResultSet resultat = stat.executeQuery("SELECT * FROM plat");
+            while (resultat.next()) {
+                System.out.println("Id: " + resultat.getInt("id")
+                        + ", Nom: " + resultat.getString("nom")
+                        + ", Descripció: " + resultat.getString("descripcio")
+                        + ", Preu: " + resultat.getDouble("preu")
+                        + ", URL de la imatge: " + resultat.getString("urlIMG")
+                        + ", Categoría: " + resultat.getString("categoria")
+                        + ", Ingredients: " + resultat.getString("ingredients"));
+            }
+            resultat.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /*Connection conn = obtenirConnexio();
 
 
         try {
@@ -60,7 +139,7 @@ public class ConnexioMesProves {
             // System.out.println("Tabla 'plat' eliminada correctamente.");
         } finally {
             conn.close();
-        }
+        }*/
     }
 
     /**
@@ -71,7 +150,7 @@ public class ConnexioMesProves {
      */
     public static Connection obtenirConnexio() throws SQLException, IOException {
         Properties props = new Properties();
-        FileInputStream in = new FileInputStream("C:\\Users\\polsa\\OneDrive\\Escritorio\\DAW2\\M03\\UF6\\M03UF6ProjecteRestaurant\\src\\main\\resources\\database.properties");
+        FileInputStream in = new FileInputStream("C:\\Users\\Marc Pedemonte\\IdeaProjects\\M03UF6ProjecteRestaurant\\src\\main\\resources\\database.properties");
         props.load(in);
         in.close();
 
